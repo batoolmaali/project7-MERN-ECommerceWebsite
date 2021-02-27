327dc2d656ffe6b163070af30b9a9f419e8f7651
unknown <batool.s.maali@gmail.com>
2021-02-27 23:39:56 +0300

    project 7 Mernstack

---
+++ build/precache-manifest.89f112b4eee6ad6a4491e6f4e13c8442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0438e02da0b59eb6e460bc81bdfee64b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9f226cdbc9689ba99b",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "376b912da57d6203941d",
+    "url": "/static/js/2.80bab748.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.80bab748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9f226cdbc9689ba99b",
+    "url": "/static/js/main.4cab42eb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
